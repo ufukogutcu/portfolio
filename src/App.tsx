463cf9f744de2dac7cd3a2dea65e2c10a6353aba
unknown <ufukogutcu@gmail.com>
2023-08-27 17:50:43 +0100

    Added Navbar and page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 
-import { Button } from "@nextui-org/react";
+import MyNavbar from "./MyNavbar";
+
+import MainPage from "./pages/MainPage";
+import TechStackPage from "./pages/TechStackPage";
+import ProjectsPage from "./pages/ProjectsPage";
 
 function App() {
+  const [page, setPage] = useState("Main");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Button color="primary">Button</Button>
-    </div>
+    <>
+      <MyNavbar
+        setPage={(page) => {
+          setPage(page);
+        }}
+      ></MyNavbar>
+      {page === "Main" && <MainPage></MainPage>}
+      {page === "TechStack" && <TechStackPage></TechStackPage>}
+      {page === "Projects" && <ProjectsPage></ProjectsPage>}
+    </>
   );
 }
 
---
+++ src/pages/TechStackPage.tsx
@@ -0,0 +1,5 @@
+function TechStackPage() {
+  return <div>TechStackPage</div>;
+}
+
+export default TechStackPage;
