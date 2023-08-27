288ef7facd891d4f2b5e1bbd62bffdd5f015731a
unknown <ufukogutcu@gmail.com>
2023-08-27 16:31:51 +0100

    Installed NextUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+
+import { Button } from "@nextui-org/react";
 
 function App() {
   return (
@@ -19,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Button color="primary">Button</Button>
     </div>
   );
 }
