01e2bd7ddee0189f9553a709d30a659dc32e5251
unknown <ufukogutcu@gmail.com>
2023-09-02 15:27:02 +0100

    Added basic skills

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -1,46 +1,30 @@
-import logo from "../static/linkedin.png";
+//Default
+import logo from "../static/skills/react.png";
+//Frontend
+import react from "../static/skills/react.png";
+import javascript from "../static/skills/javascript.png";
+import typescript from "../static/skills/typescript.png";
+import html from "../static/skills/html.png";
+import css from "../static/skills/css.png";
+//Backend
+import python from "../static/skills/python.png";
+import django from "../static/skills/django.png";
+import flask from "../static/skills/flask.png";
+//Others
+import git from "../static/skills/git.png";
 
 export let frontend_skills = [
-  [logo, "cc"],
-  [logo, "cc"],
+  [react, "React"],
+  [javascript, "JavaScript"],
+  [typescript, "TypeScript"],
+  [html, "Html"],
+  [css, "Css"],
 ];
 
 export let backend_skills = [
-  [logo, "Flask"],
-  [logo, "cc"],
-  [logo, "cc"],
-  [logo, "cc"],
-  [logo, "cc"],
-  [logo, "cc"],
-  [logo, "cc"],
-  [logo, "cc"],
-  [logo, "cc"],
-  [logo, "cc"],
-  [logo, "cc"],
-  [logo, "cc"],
-  [logo, "cc"],
-  [logo, "cc"],
-  [logo, "cc"],
-  [logo, "cc"],
-  [logo, "cc"],
-  [logo, "cc"],
-  [logo, "cc"],
-  [logo, "cc"],
-  [logo, "cc"],
-  [logo, "cc"],
-  [logo, "cc"],
-  [logo, "cc"],
-  [logo, "cc"],
-  [logo, "cc"],
-  [logo, "cc"],
-  [logo, "cc"],
-  [logo, "cc"],
-  [logo, "cc"],
-  [logo, "cc"],
-  [logo, "cc"],
-  [logo, "cc"],
-  [logo, "cc"],
-  [logo, "cc"],
+  [python, "Python"],
+  [django, "Django"],
+  [flask, "Flask"],
 ];
 
-export let other_skills = [[logo, "cc"]];
+export let other_skills = [[git, "Git"]];
