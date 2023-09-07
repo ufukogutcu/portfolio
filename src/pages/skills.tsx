8748f5b75afe9c778a0646e6320dad68cb43a6c3
unknown <ufukogutcu@gmail.com>
2023-09-07 12:31:10 +0100

    Added some tech stack skills

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -6,12 +6,29 @@ import javascript from "../static/skills/javascript.png";
 import typescript from "../static/skills/typescript.png";
 import html from "../static/skills/html.png";
 import css from "../static/skills/css.png";
+import tailwindcss from "../static/skills/tailwindcss.png";
 //Backend
 import python from "../static/skills/python.png";
 import django from "../static/skills/django.png";
 import flask from "../static/skills/flask.png";
+import sql from "../static/skills/sql.png";
+import postgresql from "../static/skills/postgresql.png";
+import sqlite from "../static/skills/sqlite.png";
+import celery from "../static/skills/celery.png";
+import rabbitmq from "../static/skills/rabbitmq.png";
 //Others
 import git from "../static/skills/git.png";
+import docker from "../static/skills/git.png";
+import nginx from "../static/skills/git.png";
+import gcp from "../static/skills/git.png";
+import java from "../static/skills/git.png";
+import c from "../static/skills/git.png";
+import cpp from "../static/skills/git.png";
+import cs from "../static/skills/git.png";
+import solidity from "../static/skills/git.png";
+import haskell from "../static/skills/git.png";
+import unity from "../static/skills/git.png";
+import procreate from "../static/skills/git.png";
 
 export let frontend_skills = [
   [react, "React"],
@@ -19,12 +36,31 @@ export let frontend_skills = [
   [typescript, "TypeScript"],
   [html, "Html"],
   [css, "Css"],
+  [tailwindcss, "Tailwind CSS"],
 ];
 
 export let backend_skills = [
   [python, "Python"],
   [django, "Django"],
   [flask, "Flask"],
+  [sql, "SQL"],
+  [postgresql, "PostgreSQL"],
+  [sqlite, "SQLite"],
+  [celery, "Celery"],
+  [rabbitmq, "RabbitMQ"],
 ];
 
-export let other_skills = [[git, "Git"]];
+export let other_skills = [
+  [git, "Git"],
+  [docker, "Docker"],
+  [nginx, "Nginx"],
+  [gcp, "GCP"],
+  [java, "Java"],
+  [c, "C"],
+  [cpp, "C++"],
+  [cs, "C#"],
+  [solidity, "Solidity"],
+  [haskell, "Haskell"],
+  [unity, "Unity"],
+  [procreate, "Procreate"],
+];
